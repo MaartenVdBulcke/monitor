aafd1d79e2422875253935069e2086c2d0e0202b
maarten.vdbul <maarten.vdbul@gmail.com>
2023-03-01 00:06:54 +0100

    🐛 Catch database connection failed error & provide temp alternative

--- see.py
+++ see.py
@@ -1,11 +1,19 @@
 import streamlit as st
+from sqlalchemy.exc import OperationalError as SqlAlchemyError
+from pymysql.err import OperationalError as PyMySqlError
 
 from db import MariaEngine
 from monitor import Monitor
 
 st.set_page_config(page_title='Loof monitor', page_icon='🛸')
 
-engine = MariaEngine.get_engine()
-Monitor.update(engine)
+try:
+    engine = MariaEngine.get_engine()
+    Monitor.update(engine)
+except (SqlAlchemyError, PyMySqlError):
+    st.write('Connection to database failed.\nSoon you will be seeing a backup here.')
+    # TODO: create a local sqlite3 db:
+        # 1. as backup for data
+        # 2. as backup for rendering
 
 st.button('REFRESH')
