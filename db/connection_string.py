63d65b557a85e0837665fde849c9bcef51e68b84
maarten.vdbul <maarten.vdbul@gmail.com>
2023-02-28 21:50:57 +0100

    ✨ Add db module

---
+++ db/connection_string.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+def get_connection_string() -> str:
+
+    st.write(st.secrets.keys(), unsafe_allow_html=True)
+
+    db_type = st.secrets['db_type']
+    username = st.secrets['username']
+    password = st.secrets['password']
+    host = st.secrets['host']
+    port = st.secrets['port']
+    db_name = st.secrets['db_name']
+
+    return f'{db_type}://{username}:{password}@{host}:{port}/{db_name}'
