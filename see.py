6ec4c1ae104b4b610c3f651aec4a3e247feb314b
maarten.vdbul <maarten.vdbul@gmail.com>
2023-02-28 21:51:46 +0100

    ♻ Refactor see.py

--- see.py
+++ see.py
@@ -1,26 +1,7 @@
-from sqlalchemy import create_engine
-from pandas import read_sql
-import plotly.express as px
-import streamlit as st
-import pymysql
+from db import MariaEngine
+from monitor import Monitor
 
-db_type = st.secrets['db_type']
-user = st.secrets['username']
-password = st.secrets['password']
-database = st.secrets['db_name']
-host = st.secrets['host']
-port = st.secrets['port']
 
-engine = create_engine(f'{db_type}://{user}:{password}@{host}:{port}/{database}')
-
-with engine.connect() as connection:
-    measurements = read_sql('measurements', connection)
-
-fig = px.line(
-    measurements,
-    x='timestamp_utc',
-    y='temperature',
-    title='Temperature kids'
-)
-
-st.plotly_chart(fig, use_container_width=True)
+engine = MariaEngine.get_engine()
+measurements = Monitor.read_database(engine)
+Monitor.render(measurements)
