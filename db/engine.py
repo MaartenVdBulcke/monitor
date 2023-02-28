e37a3bffe516836f8fb9c41cdfd3ab18f762cb9f
maarten.vdbul <maarten.vdbul@gmail.com>
2023-02-28 22:14:38 +0100

    🧨 Try caching decorators

--- db/engine.py
+++ db/engine.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 
@@ -7,6 +8,6 @@ from .connection_string import get_connection_string
 class MariaEngine:
 
     @staticmethod
+    @st.cache_resource
     def get_engine() -> Engine:
         return create_engine(get_connection_string())
-
