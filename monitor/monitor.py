cd5eae8b2c7515996b8b9b4d19efba27e34eac0b
maarten.vdbul <maarten.vdbul@gmail.com>
2023-02-28 23:54:31 +0100

    ⏲ Correct plot time to local time

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,5 +1,5 @@
 from sqlalchemy.engine import Engine
-from pandas import read_sql, DataFrame
+from pandas import read_sql, DataFrame, Timedelta
 import plotly.express as px
 import streamlit as st
 
@@ -7,11 +7,23 @@ import streamlit as st
 class Monitor:
 
     @staticmethod
+    def update(engine: Engine) -> None:
+        measurements = Monitor.read_database(engine)
+        measurements = Monitor.to_local_time(measurements)
+        Monitor.render(measurements)
+
+    @staticmethod
     def read_database(engine: Engine) -> DataFrame:
         with engine.connect() as connection:
             return read_sql('measurements', connection)
 
     @staticmethod
+    def to_local_time(df: DataFrame) -> DataFrame:
+        df.timestamp_utc += Timedelta(hours=1)
+        return df
+
+    @staticmethod
     def render(df: DataFrame) -> None:
+
         fig = px.line(df, x='timestamp_utc', y='temperature', title='Temperature kids')
         st.plotly_chart(fig, use_container_width=True)
