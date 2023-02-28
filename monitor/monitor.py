e7caf11c294f61a0d0989107e6560d1427ceceeb
maarten.vdbul <maarten.vdbul@gmail.com>
2023-02-28 21:51:21 +0100

    ✨ Add monitor module

---
+++ monitor/monitor.py
@@ -0,0 +1,17 @@
+from sqlalchemy.engine import Engine
+from pandas import read_sql, DataFrame
+import plotly.express as px
+import streamlit as st
+
+
+class Monitor:
+
+    @staticmethod
+    def read_database(engine: Engine) -> DataFrame:
+        with engine.connect() as connection:
+            return read_sql('measurements', connection)
+
+    @staticmethod
+    def render(df: DataFrame) -> None:
+        fig = px.line(df, x='timestamp_utc', y='temperature', title='Temperature kids')
+        st.plotly_chart(fig, use_container_width=True)
