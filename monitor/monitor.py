568084116f63f09efc5d3e2dd644ec48b565c997
maarten.vdbul <maarten.vdbul@gmail.com>
2023-03-04 14:11:33 +0100

    💄 Add last measurement info, small plotting changes

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -15,32 +15,40 @@ class Monitor:
 
         df = Monitor.to_local_time(df)
 
+        cls.show_latest_measurement(df)
+
         fig_day = cls.define_plot_day(df)
         st.plotly_chart(fig_day, use_container_width=True)
 
-        fig = cls.define_plot(df)
-        st.plotly_chart(fig, use_container_width=True)
-
-    @staticmethod
-    def define_plot(df: DataFrame):
-        bar = px.bar(df, x='timestamp', y='temperature', title='Temperature kids')
-        bar.update_traces(marker_color='green')
-        bar.update_layout(
-            title_x=0.38,
-            title_font_size=30,
-            xaxis=dict(tickformat='%H:%M <br>%A')
+    @classmethod
+    def show_latest_measurement(cls, df: DataFrame):
+        latest_measurement_time = df.timestamp.iloc[-1]
+        latest_measurement_temp = df.temperature.iloc[-1]
+        st.markdown(
+            f'<h2 style=text-align:center;color:darkTurquoise;>'
+            f'{round(latest_measurement_temp, 1)}° Celsius'
+            f'</h2>',
+            unsafe_allow_html=True
         )
-        return bar
+        if (m := latest_measurement_time.minute) < 10:
+            m = f'0{m}'
+
+            st.markdown(
+                f'<h5 style=text-align:center;color:darkTurquoise;>'
+                f'last measured at {latest_measurement_time.hour}h{m}'
+                f'</h5>',
+                unsafe_allow_html=True
+            )
 
     @classmethod
     def define_plot_day(cls, df: DataFrame):
         bar = px.bar(
-            df, x='timestamp', y='temperature', title='Temperature kids now',
+            df, x='timestamp', y='temperature', title=' ', # title='Temperature kids',
             range_x=[df.timestamp.iloc[-1] - Timedelta(hours=5), df.timestamp.iloc[-1] + Timedelta(hours=0.25)]
         )
         bar.update_traces(marker_color='red')
         bar.update_layout(
-            title_x=0.38,
+            # title_x='center',
             title_font_size=30,
             xaxis=dict(tickformat='%H:%M <br>%A')
         )
